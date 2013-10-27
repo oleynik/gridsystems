74c20d5a4c5dd92e25c0de6b47710a80ec6dc120
infinity <seti.post@gmail.com>
2013-10-27 12:14:04 +0200

    Make small modification

--- lab-1/src/main/java/ua/infinity/studies/gridsystems/lab1/Main.java
+++ lab-1/src/main/java/ua/infinity/studies/gridsystems/lab1/Main.java
@@ -25,8 +25,7 @@ public class Main {
      * */
     public static void main(String[] args) {
         IntegerBinaryTransformer transformer = new IntegerBinaryTransformer();
-        for (int i = -8; i < 8; i++) {
-            int decimal = i;
+        for (int decimal = -8; decimal < 8; decimal++) {
             String binary = transformer.transform(decimal);
             logger.info("Dec={}, Bin={}", decimal, binary);
         }
